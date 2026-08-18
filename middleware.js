8c522e14a16a1bf7efefa73fb51ab3661fea356c
Aayush Kumar Singh <aayushkumarsingh548@gmail.com>
2026-08-18 12:22:10 +0530

    fixed minor issues

--- middleware.js
+++ middleware.js
@@ -46,12 +46,6 @@ module.exports.validateReviewReply = (req, res, next) => {
 module.exports.isLoggedIn = (req, res, next) => {
     if (!req.isAuthenticated()) {
         req.session.redirectUrl = req.originalUrl;
-
-        req.flash(
-            "error",
-            "You must be logged in to access this page"
-        );
-
         return res.redirect("/login");
     }
 
